2dea34af0895a75fb055ec2f5e0030c2b6da2a69
Ngahu <jamaalaraheem@gmail.com>
2016-10-11 12:51:10 +0300

    adding of the project structure and also the config of the project

--- mysite/MainApp/views.py
+++ mysite/MainApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django import template
+from django.template import loader
 
-# Create your views here.
+def index(request):
+    template = loader.get_template('MainApp/index.html')
+
+    return HttpResponse(template.render(request))
+
